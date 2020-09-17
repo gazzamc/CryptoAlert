40596f46ddfeb687f3658363cff09d935795fe8f
gazzamc <gazzaplays@gmail.com>
2020-09-18 00:19:42 +0100

    Initial Commit

---
+++ cryptoalarm/cryptoalarm/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cryptoalarm project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+from dotenv import load_dotenv
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cryptoalarm.settings')
+
+application = get_wsgi_application()
